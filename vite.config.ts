2116f5e19163103851201f4186f57fb674e42b6f
Alexi Kehayias <ajkehayias@gmail.com>
2025-08-15 12:22:11 +0200

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/alexi-ai-artistry/", 
+  base: "/alexikeahyiasportflio/", 
   server: {
     host: "::",
     port: 8080,
